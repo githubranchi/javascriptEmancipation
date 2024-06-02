d91c67e6b0cc8ac6513c97ae5ddd99dc7170f314
githubranchi <asadhoda021@gmail.com>
2024-06-02 04:58:27 +0000

    slice, splice, array flatning

---
+++ arr_slice_splice.js
@@ -0,0 +1,10 @@
+// const home = ["home" , "car" , "bike", "houses" , "flats" , "apartments" , "societies" , "clubs"];
+
+// const myArr = home.slice(1, 6)
+
+// console.log(home.slice(1, 6));
+
+// home.splice(2, 5 , "Asad")
+
+// console.log(home);
+
---
+++ sort_array.js
@@ -0,0 +1,6 @@
+const arr = ["Ranchi", "Bokaro", "Dhanbad", "Patratu"];
+arr.sort();
+console.log(arr);
+
+const myarr1 = ["120", "120.12", "545", "520"];
+console.log(myarr1.toSorted());
